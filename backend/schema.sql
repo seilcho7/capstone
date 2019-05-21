4e3c6ab0ca8c1044e4f94133a6c310617fe47fab
Ashish Garg <mragarg@gmail.com>
2019-05-21 11:49:16 -0400

    Updates schema and seed

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,11 +1,19 @@
+create table host (
+    id serial primary key, 
+    roomId varchar(100)
+);
+
 create table users (
     id serial primary key,
+    room_id varchar(100),
+    name varchar(100),
     points integer,
-    name varchar(100)
+    answer varchar(300)
 );
 
 create table drawing (
     id serial primary key,
+    room_id varchar(100),
     user_id integer references users(id),
     drawing_data text
 );
