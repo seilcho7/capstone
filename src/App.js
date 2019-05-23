43e37ab5a22cf9972ba72f6fe6b441ebab6f3e0d
Ashish Garg <mragarg@gmail.com>
2019-05-23 15:46:27 -0400

    Hides my mistakes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import HostOrJoin from './components/HostOrJoin';
 import HowToPlay from './components/HowToPlay';
 import WaitPage from './components/WaitPage';
 import Answer from './components/Answer';
-import SubmitAnswer from './components/SubmitAnswer';
+// import SubmitAnswer from './components/SubmitAnswer';
 
 class App extends React.Component {
 
@@ -134,9 +134,9 @@ class App extends React.Component {
         <Route path="/answer" component={(props) => (
             <Answer {...props} answerChoices={this.state.answerChoices} />
           )}/>
-        <Route path="/submitanswer" component={(props) => (
+        {/* <Route path="/submitanswer" component={(props) => (
             <SubmitAnswer {...props} submitAnswer={this._addAnswerChoice}/>
-          )}/>
+          )}/> */}
         <Route path='/host-or-join' render={(props) => (
             <HostOrJoin {...props} handleClickHost={this._setPin} />
           )} />
