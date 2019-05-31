e39c78f14fd75343e40d55a3464f38d2e9c732de
Seil Cho <seilcho7@hotmail.com>
2019-05-31 11:14:55 -0400

    adds build

---
+++ build/precache-manifest.64d8996605b533b62eb611e72ca006dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ffff9713c26e927226ea7aa757c443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d76ddc696b5eae1ba8e1",
+    "url": "/static/css/main.db7bb562.chunk.css"
+  },
+  {
+    "revision": "9d1c4a84b3adc901fa8f",
+    "url": "/static/js/2.5ea80e03.chunk.js"
+  },
+  {
+    "revision": "d76ddc696b5eae1ba8e1",
+    "url": "/static/js/main.d98f957f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ec348c81fa9352f893625de5d7a8402",
+    "url": "/static/media/picme-logo.0ec348c8.png"
+  }
+]);
